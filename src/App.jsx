cc8b3da17d9736a3b5055fdd9b20465382254eb5
Gabriel Pereira Martins <gabriel.m.ext@sascar.com.br>
2021-09-20 15:52:31 -0300

    Criacao do projeto

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.scss'
+
+export default (props) => (
+    <div className="App">
+
+    </div>
+)
+
+pokemons: [
+    {
+        nationalDex: '',
+        name:        '',
+        image:       '',
+        linkPkmDB:   '',
+        nextEvo:     '',
+        avaiableGames: [
+            {
+                code: 
+            }
+        ]
+    }
+]
